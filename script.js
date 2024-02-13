d73dfd79e8df0bfcb8a1c67096a437d8acd753fb
Vanesa Edo <vanesa.en@gmail.com>
2024-02-13 12:50:16 +0100

    Apuntes de la explicación de Objetos en clase

---
+++ script.js
@@ -0,0 +1,25 @@
+//-------OBJETOS-------
+
+const person = {
+    firstname : "Vanesa",
+    age : 46,
+    return this.age; //* 
+}
+
+
+
+nombreObjeto["nombre-Clave"]//esta forma de solicitar el contenido de una clave cuando el nombre de la clave está entrecomillado. Esto ocurre a menudo si la fuente es una api y no sabemos cómo se han nombrado las claves.
+nombreObjeto.nombreclave 
+//Si nombre un valor que es una función
+nombreClave
+ 
+typeof {} //Se dice que es un json (no están iterados y tienen nombre)
+typeof [] //SE dice que es un array (tiene posiciones y se puede iterar)
+
+cars[1].model //estoy accediendo a un objeto que es un array, y dentro de ese array que es una lista de propiedades con sus clave-valror, accede mos al valor de la clave llamada model.
+
+//Mirar Extracting String Methods of String 
+//split ("")Utiliza lo que dices que hay entre las comillas como indicador de por dónde cortar y elimina eso que indicas. 
+//join ("") lo que pones entre las comillas es lo que pondrá entre ítem e ítem cuando lo convierta en un string.
+
+//API: Aplicaciones que proporcionan datos. Tienen su propia documentación.
